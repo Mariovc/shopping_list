e9434257604d0e002e515d558e93052897e66730
DESKTOP-0S94A87\Mario <mariovelasco3@hotmail.com>
2023-09-10 15:31:15 +0200

    Add new item screen using a Form()

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,19 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/widgets/grocery_list_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
   @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const NewItem(),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: groceryItems.length,
-      itemBuilder: (context, index) {
-        return GroceryListItem(
-          item: groceryItems[index],
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          return GroceryListItem(
+            item: groceryItems[index],
+          );
+        },
+      ),
     );
   }
 }
